cd9151feaee4e311e7ca7c33d55079c18b49713d
XueyingChen <115363697+Dkaattae@users.noreply.github.com>
2025-12-18 19:34:28 +0000

    dlt

---
+++ adv-form/debug_script.py
@@ -0,0 +1,129 @@
+import dlt
+import requests
+import zipfile
+import io
+import pandas as pd
+from typing import Iterator
+
+from util import generate_months
+from util import filing_dates
+from util import build_url
+
+def generate_csv_table(year: int, month: int):
+    yyyyMMdd = filing_dates(year, month)
+    yyyyMM = yyyyMMdd[:-2]
+    CSV_TABLES = {
+        f"IA_Schedule_A_B_{yyyyMM}01_{yyyyMMdd}.csv": {
+            "table": "Filings",
+            "primary_key": "FilingID",
+        },
+        f"IA_Schedule_D_7B1_{yyyyMM}01_{yyyyMMdd}.csv": {
+            "table": "Funds",
+            "primary_key": ["FilingID", "Fund ID", "ReferenceID"]
+        },
+        f"IA_Schedule_D_7B1A6b_{yyyyMM}01_{yyyyMMdd}.csv": {
+            "table": "Advisor_Fund_Relationships",
+            "primary_key": ["FilingID", "ReferenceID"]
+        },
+        f"IA_Schedule_D_7A_CIK_{yyyyMM}01_{yyyyMMdd}.csv": {
+            "table": "CIKMap",
+            "primary_key": ["FilingID", "ReferenceID"],
+        },
+        f"IA_Schedule_D_7B1A22_{yyyyMM}01_{yyyyMMdd}.csv": {
+            "table": "FormDNum",
+            "primary_key": ["FilingID", "ReferenceID"],
+        },
+        f"IA_Schedule_D_7B1A23_{yyyyMM}01_{yyyyMMdd}.csv": {
+            "table": "Auditors",
+            "primary_key": ["FilingID", "ReferenceID"],
+        },
+        f"IA_Schedule_D_7B1A24_{yyyyMM}01_{yyyyMMdd}.csv": {
+            "table": "Primary_Brokers",
+            "primary_key": ["FilingID", "ReferenceID"],
+        },
+        f"IA_Schedule_D_7B1A25_{yyyyMM}01_{yyyyMMdd}.csv": {
+            "table": "Custodians",
+            "primary_key": ["FilingID", "ReferenceID"],
+        },
+        f"IA_Schedule_D_7B1A26_{yyyyMM}01_{yyyyMMdd}.csv": {
+            "table": "FundAdmins",
+            "primary_key": ["FilingID", "ReferenceID"],
+        },
+        f"IA_Schedule_D_7B1A28_{yyyyMM}01_{yyyyMMdd}.csv": {
+            "table": "Marketers",
+            "primary_key": ["FilingID", "ReferenceID"],
+        },
+    }
+    return CSV_TABLES
+
+
+@dlt.source
+def adv_filing_source(url: str, year: int, month: int):
+
+    CSV_TABLES = generate_csv_table(year, month)
+
+    def load_zip():
+        response = requests.get(url)
+        response.raise_for_status()
+        return zipfile.ZipFile(io.BytesIO(response.content))
+
+    zip_file = load_zip()
+
+    resources = []
+
+    for csv_name, cfg in CSV_TABLES.items():
+
+        @dlt.resource(
+            name=cfg["table"],
+            primary_key=cfg["primary_key"],
+            write_disposition="merge",
+        )
+        def csv_resource(
+            csv_name=csv_name,
+        ) -> Iterator[dict]:
+            with zip_file.open(csv_name) as f:
+                df = pd.read_csv(f)
+                for row in df.to_dict(orient="records"):
+                    yield row
+
+        resources.append(csv_resource)
+
+    return resources
+
+def backfill():
+    pipeline = dlt.pipeline(
+        pipeline_name="adv_filing",
+        destination="postgres",
+        dataset_name="sec_adv",
+    )
+
+    for (year, month) in generate_months(12, 1):
+        url = build_url(year, month)
+        print(f"Loading {url}")
+        try:
+            pipeline.run(
+                adv_filing_source(url, year, month),
+                loader_file_format="csv",
+                write_disposition="replace"
+            )
+        except requests.exceptions.HTTPError:
+            print(f"Data or {year}-{month:02d} not found")
+
+def monthly_run():
+    pipeline = dlt.pipeline(
+        pipeline_name="adv_filing",
+        destination="postgres",
+        dataset_name="sec_adv",
+    )
+
+    last_month = date.today().replace(day=1) - relativedelta(months=1)
+    url = build_url(last_month)
+
+    pipeline.run(
+        adv_filing_source(url, year, month),
+        loader_file_format="csv",
+        write_disposition="replace"
+    )
+
+if __name__ == "__main__":
+    backfill()
---
+++ adv-form/util.py
@@ -0,0 +1,44 @@
+from datetime import date
+from dateutil.relativedelta import relativedelta
+import calendar
+
+def generate_months(start_months_ago=12, end_months_ago=1, current_year_month=None):
+    
+    if current_year_month:
+        today = date(current_year_month[0], current_year_month[1], 1)
+    else:
+        today = date.today().replace(day=1)
+    start = today - relativedelta(months=start_months_ago)
+    end = today - relativedelta(months=end_months_ago)
+
+    months = []
+    current = start
+    while current <= end:
+        months.append((current.year, current.month))
+        current += relativedelta(months=1)
+
+    return reversed(months)
+
+def filing_dates(year, month):
+    last_day = calendar.monthrange(year, month)[1]
+    yyyyMM = f"{year}{month:02d}"
+    yyyyMMdd = f"{year}{month:02d}{last_day:02d}"
+    return yyyyMMdd
+
+
+def build_url(year, month):
+    yyyyMMdd = filing_dates(year, month)
+    year = int(yyyyMMdd[:4])
+    yyyyMM = yyyyMMdd[:-2]
+    
+    return (
+        f"https://reports.adviserinfo.sec.gov/reports/foia/"
+        f"advFilingData/{year}/"
+        f"ADV_Filing_Data_{yyyyMM}01_{yyyyMMdd}.zip"
+    )
+
+if __name__ == "__main__":
+    url = build_url(2025, 10)
+    # print(url)
+    months = generate_months(12, 1, (2025,11))
+    # print(months)
